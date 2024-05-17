351c99fb9e16e2be661572383e5da754cf9630ac
Serkalem Delelegn <serkalemdelelegn2020@gmail.com>
2024-05-17 22:52:41 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,18 @@
+// import javax.swing.JFrame;
+import java.awt.*;
+
+
+
+
+public class Main{
+
+    public static void main(String[] args) {
+        Frame f=new Frame();
+        f.setVisible(true);
+        f.setSize(300,300);
+
+
+       
+
+    }
+}
