c502f27329f48b15e4928db7d3d853d336853fa9
Serkalem Delelegn <serkalemdelelegn2020@gmail.com>
2024-05-18 00:40:34 +0300

    Update FrameDemo.java

--- FrameDemo.java
+++ FrameDemo.java
@@ -1,40 +1,50 @@
 import java.awt.*;
 import java.awt.event.*;
 
+// Define a class named FrameDemo that extends the Frame class and implements the WindowListener interface
 class FrameDemo extends Frame implements WindowListener {
+    // Constructor of FrameDemo class
     FrameDemo() {
-        // Making the frame visible
+        // Make the frame visible
         setVisible(true);
         
-        // Setting the size of the frame
+        // Set the size of the frame to 300x300 pixels
         setSize(300, 300);
         
-        // Setting the title of the frame
+        // Set the title of the frame to "Window Listener"
         setTitle("Window Listener");
         
-        // Adding the listener to the frame
+        // Add this FrameDemo object as a window listener to handle window events
         addWindowListener(this);
     }
 
+    // Main method - entry point of the application
     public static void main(String[] args) {
-        // Creation of frame object
+        // Create an instance of FrameDemo which will display the frame
         FrameDemo fd = new FrameDemo();
     }
 
-    // Null implementation methods
+    // Called when the window is first opened
     public void windowOpened(WindowEvent we) {}
 
+    // Called when the user attempts to close the window
     public void windowClosing(WindowEvent we) {
+        // Terminate the program
         System.exit(0);
     }
 
+    // Called when the window has been closed
     public void windowClosed(WindowEvent we) {}
 
+    // Called when the window is activated (gains focus)
     public void windowActivated(WindowEvent we) {}
 
+    // Called when the window is deactivated (loses focus)
     public void windowDeactivated(WindowEvent we) {}
 
+    // Called when the window is minimized (iconified)
     public void windowIconified(WindowEvent we) {}
 
+    // Called when the window is restored from minimized state
     public void windowDeiconified(WindowEvent we) {}
 }
